94bab7346263f21900e701000579fdeb119ad86c
Wellington Perez <wellingtonaperez@gmail.com>
2017-07-28 18:03:41 -0300

    Adding model task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,27 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app works!';
+  title = 'Gerenciador de Tarefas';
+
+  // Ex sem construtor
+  task: Task = {
+    id: 2,
+    title: 'Enviar orçamento para o ciente XYZ'
+  };
+
+  task2: Task = new Task(21, 'Enviar orçamento para o ciente X');
+
+
+}
+
+export class Task {
+  public id: number;
+  public title: string;
+
+  constructor(id: number, title: string) {
+    this.id = id;
+    this.title = title;
+  }
 }
