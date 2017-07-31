42d7d1f04a27c325519c7c21a46ec3fbf3c73ff5
Wellington Perez <wellingtonaperez@gmail.com>
2017-07-31 17:44:21 -0300

    Moving the task model to the correct place

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,22 +10,10 @@ export class AppComponent {
   title = 'Gerenciador de Tarefas';
 
   // Ex sem construtor
-  task: Task = {
-    id: 2,
-    title: 'Enviar orçamento para o ciente XYZ'
-  };
+  // task: Task = {
+  //   id: 2,
+  //   title: 'Enviar orçamento para o ciente XYZ'
+  // };
 
-  task2: Task = new Task(21, 'Enviar orçamento para o ciente X');
-
-
-}
-
-export class Task {
-  public id: number;
-  public title: string;
-
-  constructor(id: number, title: string) {
-    this.id = id;
-    this.title = title;
-  }
+  // task2: Task = new Task(21, 'Enviar orçamento para o ciente X');
 }
