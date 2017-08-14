a4ed6a58ba14a05b8b830e282ce2bcd66db6efce
Wellington Perez <wellingtonaperez@gmail.com>
2017-08-14 14:40:06 -0300

    Adding route to TaskDetailComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,10 @@ import { TaskService } from './tasks/shared/task.service';
 
 const ROUTES = RouterModule.forRoot([
   {
+    path: 'tasks/:id',
+    component: TaskDetailComponent
+  },
+  {
     path: 'dashboard',
     component: DashboardComponent
   },
